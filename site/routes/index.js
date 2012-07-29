7ee29472a480e2b8f6d6ddc7cf74e66ad276907c
Pascal Garber <jumplink@gmail.com>
2012-07-30 00:31:24 +0200

    express with bootstrap, own index and github

---
+++ site/routes/index.js
@@ -0,0 +1,28 @@
+
+/*
+ * GET home page.
+ */
+var Showdown = require('showdown');
+var converter = new Showdown.converter();
+var fetchUrl = require("fetch").fetchUrl;
+
+function get_readme(return_readme) {
+  fetchUrl("https://raw.github.com/JumpLink/HMWorld/master/README.md", function(error, meta, body){
+    cb(converter.makeHtml(body.toString()));
+  });
+}
+
+function return_readme(html) {
+  return html
+}
+
+
+exports.index = function(req, res){
+  fetchUrl("https://raw.github.com/JumpLink/HMWorld/master/README.md", function(error, meta, body){
+    var readme = converter.makeHtml(body.toString());
+    res.render('index', {
+      title: 'HMWorld',
+      readme:readme
+    });
+  });
+};
